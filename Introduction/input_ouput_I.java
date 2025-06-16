d55da2566dee0a6eb3228c4e99855e2041ffd6c7
Omi Mishra <work.omimishra@gmail.com>
2025-06-16 20:10:35 +0530

    #Introduction:Input output basic

---
+++ Introduction/input_ouput_I.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+//https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem?isFullScreen=true
+public class input_ouput_I {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int c = scan.nextInt();
+        int d = scan.nextInt();
+       
+        scan.close();
+        
+        // Complete this line
+        // Complete this line
+
+       
+        System.out.println( a);
+        System.out.println( c);
+        System.out.println( d);
+        // Complete this line
+        // Complete this line
+    }
+}
