fdf51a68415f3dbef435b84994f534c06f38d6f8
Omi Mishra <work.omimishra@gmail.com>
2025-08-24 21:21:03 +0530

    #Big Decimal:Add prime number checker using BigInteger class

---
+++ BigDecimal/prime.java
@@ -0,0 +1,24 @@
+package BigDecimal;
+
+import java.io.*;
+import java.math.*;
+
+public class prime {
+    // Given a large integer, n , use the Java BigInteger class' isProbablePrime
+    // method to determine and print whether it's prime or not prime.
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = bufferedReader.readLine();
+
+        bufferedReader.close();
+        BigInteger big = new BigInteger(n);
+
+        if (big.isProbablePrime(2)) {
+            System.out.println("prime");
+        } else {
+            System.out.println("not prime");
+        }
+    }
+}
