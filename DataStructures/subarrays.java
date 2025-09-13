043aa00fa66cf217bec471964fbf875946b97122
Omi Mishra <work.omimishra@gmail.com>
2025-09-13 16:23:18 +0530

    #DS: Negative subarray counter

---
+++ DataStructures/subarrays.java
@@ -0,0 +1,27 @@
+package DataStructures;
+import java.util.*;
+
+public class subarrays {
+    // Given an array of n integers, find and print its number of negative subarrays on a new line.
+    // https://www.hackerrank.com/challenges/java-negative-subarray/problem?isFullScreen=true
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i =0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int count = 0;
+        for(int i =0;i<n;i++){
+            int sum = 0 ;
+            for(int j = i;j<n;j++){
+                sum +=arr[j];
+                if(sum<0)
+                count++;
+            }
+        }
+        System.out.println(count);
+        sc.close();
+    }
+}
