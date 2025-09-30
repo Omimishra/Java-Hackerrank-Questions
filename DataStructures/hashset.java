fccaba46f07484d69af6da0f6eaf0606b4395a54
Omi Mishra <work.omimishra@gmail.com>
2025-09-30 21:45:26 +0530

    #DS: Java Hashset

---
+++ DataStructures/hashset.java
@@ -0,0 +1,36 @@
+package DataStructures;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class hashset {
+    // Print T lines. In the Ith line, print number of unique pairs you have after
+    // taking Ith pair as input.
+    // https://www.hackerrank.com/challenges/java-hashset/problem?isFullScreen=true
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String[] pair_left = new String[t];
+        String[] pair_right = new String[t];
+
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
+
+        // Write your code here
+        // HashSet<String> ans = new HashSet<>();
+        // for (int i = 0)
+        HashSet<String> pairs = new HashSet<>();
+
+        for (int i = 0; i < t; i++) {
+            String str = pair_left[i] + " " + pair_right[i];
+            String rev = pair_right[i] + " " + pair_left[i];
+            if (!pairs.contains(rev)) {
+                pairs.add(str);
+            }
+            System.out.println(pairs.size());
+        }
+        s.close();
+    }
+}
