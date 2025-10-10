95efb905ae77d98d2e4537c1d94bbbeafc6ebb32
Omi Mishra <work.omimishra@gmail.com>
2025-10-10 21:55:24 +0530

    modified code

--- DataStructures/deque.java
+++ DataStructures/deque.java
@@ -2,20 +2,24 @@ package DataStructures;
 import java.util.*;
 public class deque {
     // you are given N integers. You need to find the maximum number of unique integers among all the possible contiguous subarrays of size M.
-    
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
         int max = 0;
         Deque<Integer> dq = new ArrayDeque<>();
+        HashSet<Integer> set = new HashSet<>();
         for(int i=0;i<n;i++){
-            dq.add(sc.nextInt());
+            int num = sc.nextInt();
+            dq.add(num);
+            set.add(num);
             if(dq.size()==m){
-                int size = new HashSet<>(dq).size();
-                if(size > max)
-                max = size;
-                dq.removeFirst(); // Remove the oldest element to maintain the window size
+                max=Math.max(max, set.size());
+                int removed = dq.remove();
+                if(!dq.contains(removed)){
+                    set.remove(removed);
+                }
             }
         }
         sc.close();
